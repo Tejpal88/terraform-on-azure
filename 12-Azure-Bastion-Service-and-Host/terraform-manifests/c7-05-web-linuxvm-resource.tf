caa4c8888d2f6d01ac8aed78678976dc05f6df02
Arunbharathwaj <aarunb06@gmail.com>
2024-08-05 22:12:28 +0800

    modfied the script and resource for demo12

--- 12-Azure-Bastion-Service-and-Host/terraform-manifests/c7-05-web-linuxvm-resource.tf
+++ 12-Azure-Bastion-Service-and-Host/terraform-manifests/c7-05-web-linuxvm-resource.tf
@@ -9,11 +9,11 @@ sudo systemctl start httpd
 sudo systemctl stop firewalld
 sudo systemctl disable firewalld
 sudo chmod -R 777 /var/www/html 
-sudo echo "Welcome to stacksimplify - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/index.html
+sudo echo "Welcome to the Devops Demo by ArunB- WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/index.html
 sudo mkdir /var/www/html/app1
-sudo echo "Welcome to stacksimplify - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/app1/hostname.html
-sudo echo "Welcome to stacksimplify - WebVM App1 - App Status Page" > /var/www/html/app1/status.html
-sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to Stack Simplify - WebVM APP-1 </h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
+sudo echo "Welcome to the Devops Demo by Arun Bharathwaj A - WebVM App1 - VM Hostname: $(hostname)" > /var/www/html/app1/hostname.html
+sudo echo "Welcome to the Devops Demo by Desika Vihaan - WebVM App1 - App Status Page" > /var/www/html/app1/status.html
+sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to the Devops Demo by Arun  - WebVM APP-1 </h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
 sudo curl -H "Metadata:true" --noproxy "*" "http://169.254.169.254/metadata/instance?api-version=2020-09-01" -o /var/www/html/app1/metadata.html
 CUSTOM_DATA  
 }
@@ -22,14 +22,14 @@ CUSTOM_DATA
 # Resource: Azure Linux Virtual Machine
 resource "azurerm_linux_virtual_machine" "web_linuxvm" {
   name = "${local.resource_name_prefix}-web-linuxvm"
-  #computer_name = "web-linux-vm"  # Hostname of the VM (Optional)
+  computer_name = "webdemovm01"  # Hostname of the VM (Optional)
   resource_group_name = azurerm_resource_group.rg.name
   location = azurerm_resource_group.rg.location
   size = "Standard_DS1_v2"
-  admin_username = "azureuser"
+  admin_username = "azadmin"
   network_interface_ids = [ azurerm_network_interface.web_linuxvm_nic.id ]
   admin_ssh_key {
-    username = "azureuser"
+    username = "azadmin"
     public_key = file("${path.module}/ssh-keys/terraform-azure.pub")
   }
   os_disk {
--- 12-Azure-Bastion-Service-and-Host/terraform-manifests/terraform.tfvars
+++ 12-Azure-Bastion-Service-and-Host/terraform-manifests/terraform.tfvars
@@ -1,8 +1,8 @@
-business_divsion = "hr"
-environment = "dev"
-resource_group_name = "rg"
-resource_group_location = "eastus"
-vnet_name = "vnet"
+business_divsion = "ProjectV"
+environment = "Prod"
+resource_group_name = "ProdRg01"
+resource_group_location = "southeastasia"
+vnet_name = "ProdVnet01"
 vnet_address_space = ["10.1.0.0/16"]
 
 web_subnet_name = "websubnet"

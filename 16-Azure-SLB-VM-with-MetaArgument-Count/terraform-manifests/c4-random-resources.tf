a7a61ecd0a03332c0155cbbe4ee1ff4664db8ea9
Arunbharathwaj <aarunb06@gmail.com>
2024-08-12 10:09:07 +0800

    resolved the commit errors

--- 16-Azure-SLB-VM-with-MetaArgument-Count/terraform-manifests/c4-random-resources.tf
+++ 16-Azure-SLB-VM-with-MetaArgument-Count/terraform-manifests/c4-random-resources.tf
@@ -6,12 +6,3 @@ resource "random_string" "myrandom" {
   special = false
   numeric = false   
 }
-=======
-# Random String Resource
-resource "random_string" "myrandom" {
-  length = 6
-  upper = false 
-  special = false
-  number = false   
-}
->>>>>>> bd04ffc7bc5e587d71b7fa5b35b56f7dd068e693

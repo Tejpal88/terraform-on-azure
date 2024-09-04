1fbd88d4783e06bf0951e5d8df2b76af8e58b670
Arunbharathwaj <aarunb06@gmail.com>
2024-09-04 16:04:12 +0800

    added demoproject to prac without LB/NATWG/APLNGW

---
+++ Demo1-Azure-VM-ScaleSets-NoLB/terraform-manifests-autoscaling/c1-versions.tf
@@ -0,0 +1,26 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = ">= 2.0" 
+    }
+    random = {
+      source = "hashicorp/random"
+      version = ">= 3.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = ">= 3.0"
+    }    
+  }
+}
+
+# Provider Block
+provider "azurerm" {
+ features {}
+ subscription_id = "9855788c-b8b2-4fb1-bb28-4845a7ff9554"          
+}
+
+
